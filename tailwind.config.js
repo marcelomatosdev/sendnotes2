18a1fe3bf480c1e0bf5a7b5e78aa979d0f6cd185
marcelomatosdev <marcelomatos.dev@gmail.com>
2025-02-04 15:19:23 -0600

    tailwind updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import colors from 'tailwindcss/colors';
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -18,9 +19,11 @@ export default {
 
     theme: {
         extend: {
+        
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: { primary: colors.rose},
         },
     },
 
